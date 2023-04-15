ba0dbd394e30abe3370f4dcad0d1986835973125
interaction <243127395@qq.com>
2023-04-16 00:59:55 +0800

    update

--- nvim/lua/bufferline-config/init.lua
+++ nvim/lua/bufferline-config/init.lua
@@ -1,5 +1,9 @@
 require("bufferline").setup {
     options = {
+        indicator = {
+            icon = '▎', -- this should be omitted if indicator style is not 'icon'
+        },
+        buffer_close_icon = '✘',
     }
 }
 
