93ebd4b16c46916b7dc13b6a80e3df44b10fa16b
interaction <243127395@qq.com>
2023-05-06 15:57:37 +0800

    add hydra lualine

--- nvim/lua/lualine-config/init.lua
+++ nvim/lua/lualine-config/init.lua
@@ -1,8 +1,27 @@
 local utils = require("../lua-utils")
+local hydra = require('hydra.statusline')
+
+function hydra_status()
+    if hydra.is_active() then 
+        return string.format("[%s]", hydra.get_name())
+    end
+
+    return ""
+end
 
 require('lualine').setup({
     sections = {
-        lualine_c = { utils.get_buffer_relative_filename },
+        lualine_c = { 
+            {
+                'mode',
+                fmt = hydra_status, 
+                icon = '🐍',
+                color={bg='#105934'}
+            }, 
+            {
+                'mode',
+                fmt = utils.get_buffer_relative_filename,
+            },
+        },
     }
 })
-
