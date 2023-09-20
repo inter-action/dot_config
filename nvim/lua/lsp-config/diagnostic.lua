2709b989c58dd587668edca6f6e89ce54519d382
interaction <243127395@qq.com>
2023-09-20 15:28:36 +0800

    update

--- nvim/lua/lsp-config/diagnostic.lua
+++ nvim/lua/lsp-config/diagnostic.lua
@@ -10,10 +10,10 @@ local sign = function(opts)
     )
 end
 
-sign({name = "DiagnosticSignError", text = ""})
-sign({name = "DiagnosticSignWarn", text = ""})
-sign({name = "DiagnosticSignHint", text = ""})
-sign({name = "DiagnosticSignInfo", text = ""})
+sign({name = "DiagnosticSignError", text = "☢️"})
+sign({name = "DiagnosticSignWarn", text = "⚠︎"})
+sign({name = "DiagnosticSignHint", text = "·"})
+sign({name = "DiagnosticSignInfo", text = "I"})
 
 vim.diagnostic.config(
     {
