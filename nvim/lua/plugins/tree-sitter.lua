8f5fd4ed4a752e30571dd96b7fa3cba79f34941b
interaction <243127395@qq.com>
2024-05-24 22:13:53 +0800

    update

--- nvim/lua/plugins/tree-sitter.lua
+++ nvim/lua/plugins/tree-sitter.lua
@@ -1,6 +1,7 @@
 return {
     {
         'nvim-treesitter/nvim-treesitter',
+        lazy = false,
         build = ':TSUpdate',
         dependencies = {
             "nvim-treesitter/nvim-treesitter-textobjects"
