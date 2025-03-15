4e47d2a729f1e0c2e37f452c9052160bd7b70f2f
jimiao <243127395@qq.com>
2025-03-15 17:33:20 +0800

    update

--- nvim/lua/lua-utils/init.lua
+++ nvim/lua/lua-utils/init.lua
@@ -1,8 +1,8 @@
-local exports = {}
+local M = {}
 
 
 -- Function to retrieve and display the current cursor position
-function exports.move_cursor_next()
+function M.move_cursor_next()
 
   -- Get the current cursor position in the current window
   local current_win_id = 0
@@ -16,13 +16,13 @@ function exports.move_cursor_next()
 end
 
 
-function exports.get_buffer_relative_filename()
+function M.get_buffer_relative_filename()
     -- return string.gsub(vim.api.nvim_buf_get_name(0), vim.loop.cwd() .. '/', '')
     return vim.fn.expand('%')
 end
 
 
-function exports.merge2(a, b)
+function M.merge2(a, b)
     -- return vim.tbl_extend("force", unpack(arg))
     --                                       ^ arg is not working inside neovim
     return vim.tbl_extend("force", a, b)
@@ -30,7 +30,7 @@ end
 
 
 -- Functional wrapper for mapping custom keybindings
-function exports.map_command(mode, lhs, rhs, opts)
+function M.map_command(mode, lhs, rhs, opts)
     local options = { noremap = true }
     if opts then
         options = vim.tbl_extend("force", options, opts)
@@ -38,7 +38,7 @@ function exports.map_command(mode, lhs, rhs, opts)
     vim.api.nvim_set_keymap(mode, lhs, rhs, options)
 end
 
-function exports.map_func(mode, lhs, rhs, opts)
+function M.map_func(mode, lhs, rhs, opts)
     local options = { noremap = true }
     if opts then
         options = vim.tbl_extend("force", options, opts)
@@ -46,5 +46,18 @@ function exports.map_func(mode, lhs, rhs, opts)
     vim.keymap.set(mode, lhs, rhs, options)
 end
 
+-- using nvim_feedkeys to simulate user's input
+local function move_cursor_up(lines)
+  vim.api.nvim_feedkeys(tostring(lines) .. "k", "n", false)
+end
+
+-- Example usage: Move the cursor up by 5 lines
+-- vim.keymap.set('n', '<leader>k', function()
+--   move_cursor_up(5)
+-- end, { desc = "Move cursor up by 5 lines" })
+
+
+
+
 -- export exports
-return exports
+return M
