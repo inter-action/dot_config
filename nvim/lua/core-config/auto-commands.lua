a2cd271bcb7187f820e00c70ac6ad2f821aecb1e
interaction <243127395@qq.com>
2026-01-10 18:07:44 +0800

    update

--- nvim/lua/core-config/auto-commands.lua
+++ nvim/lua/core-config/auto-commands.lua
@@ -43,7 +43,7 @@ vim.api.nvim_create_autocmd('FocusLost', {
 vim.api.nvim_create_autocmd({ 'BufEnter', 'BufWinEnter' }, {
     group = vim.api.nvim_create_augroup('WinbarFilename', { clear = true }),
     callback = function()
-        vim.wo.winbar = vim.fn.expand('%:p'):gsub(vim.loop.cwd() .. '/', '')
+        vim.wo.winbar = vim.fn.expand('%')
     end,
     desc = 'Show relative file path in winbar',
 })
