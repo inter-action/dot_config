637081834fb33025e7b7073795a6b5d2696ca26c
interaction <243127395@qq.com>
2026-06-10 23:02:24 +0800

    update

--- nvim/lua/plugins/harpoon.lua
+++ nvim/lua/plugins/harpoon.lua
@@ -10,10 +10,12 @@ local config = function()
 
     -- 80% width
     local width = math.min(120, math.floor(vim.api.nvim_win_get_width(0) * 0.8))
+    local height = math.min(40, math.floor(vim.api.nvim_win_get_width(0) * 0.5))
     require('harpoon').setup {
         menu = {
             -- width = vim.api.nvim_win_get_width(0) - 4,
             width = width,
+            height = height
         },
     }
 
